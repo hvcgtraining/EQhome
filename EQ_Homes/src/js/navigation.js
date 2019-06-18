f40ae1722e9e6a164c077c1be6da34f2cc7be203
hvcgtraining <hvcgtraining@gmail.com>
2019-06-18 18:49:25 +0700

    fix responsive

--- EQ_Homes/src/js/navigation.js
+++ EQ_Homes/src/js/navigation.js
@@ -35,3 +35,20 @@ $(".dropdown-nav").click(function(e) {
 $('body').click(function(e) {
     $(".dropdown-nav.checked").removeClass('checked').find('.nav__list').slideUp('swing');
 })
+$('body').click(function(e) {
+    // console.log(e.target)
+    if(e.target.tagName == 'BODY' || e.target.tagName == 'form') {
+        $("body").removeClass('bg-form');
+        $(".sign-up").css({
+            opacity: 0,
+            "pointer-events": "none",
+            display: 'none'
+        });
+    }
+})
+$(".click-sign-up").click(function(){
+    $(".sign-up").css("display","flex");
+    $(".sign-up").css("opacity","1");
+    $(".sign-up").css("pointer-events","auto");
+    $("body").addClass('bg-form');
+})
