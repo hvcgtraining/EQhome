bf9e45cf13c6423c265935f1204ed53037e95d65
dongnguyen <nguyentandong0511@gmai.com>
2019-06-19 14:25:27 +0700

    add

---
+++ EQ_Homes/src/js/main-content.js
@@ -0,0 +1,58 @@
+$('.slide-content').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: false,
+    autoplay: true,
+    speed: 2000,
+    responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: false,
+            infinite: false,
+            autoplay: true,
+            speed: 2000,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            infinite: false,
+            autoplay: true,
+            speed: 2000,
+          }
+        },
+        {
+          breakpoint: 568,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            infinite: false,
+            autoplay: true,
+            speed: 2000,
+          }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+                arrows: false,
+                infinite: false,
+                autoplay: true,
+                speed: 2000,
+            }
+          }
+      ]
+});
