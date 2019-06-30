672db5675ff4350b7cb456ecd5b3060f91909808
dongnguyen <nguyentandong0511@gmai.com>
2019-06-30 17:56:26 +0700

    add

--- EQ_Homes/dist/html/pages/js/navigation.js
+++ EQ_Homes/dist/html/pages/js/navigation.js
@@ -18,22 +18,22 @@ $('body').click(function (e) {
   $(".nav__item.active").removeClass('active').find('.nav-sub__list').slideUp('swing');
 }); // 
 
-$(".dropdown-nav").click(function (e) {
+$(".menu-toggle").click(function (e) {
   e.stopPropagation(); // Ngan chan su kien click lan toa ra thang cha
   // Ẩn het toan bo nhung thang khac
 
   var navsub = $(this).find(".nav__list");
 
   if ($(this).hasClass('checked') == false) {
-    $(".dropdown-nav.checked").removeClass('checked').find('.nav__list').slideUp('swing');
+    $(".menu-toggle.checked").removeClass('checked').find('.nav__list').slideUp('swing');
     $(this).addClass('checked');
     navsub.slideDown('swing');
   } else {
-    $(".dropdown-nav.checked").removeClass('checked').find('.nav__list').slideUp('swing');
+    $(".menu-toggle.checked").removeClass('checked').find('.nav__list').slideUp('swing');
   }
 });
 $('body').click(function (e) {
-  $(".dropdown-nav.checked").removeClass('checked').find('.nav__list').slideUp('swing');
+  $(".menu-toggle.checked").removeClass('checked').find('.nav__list').slideUp('swing');
 });
 $('body').click(function (e) {
   // console.log(e.target)
